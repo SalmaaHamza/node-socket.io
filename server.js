5639b37b81ca38e71fcdd1f3d8cb3b9c63043cb3
SalmaaHamza <salmahamza108@gmail.com>
2022-04-16 07:49:39 +0200

    X

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-var users = [];
+const users = {};
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
